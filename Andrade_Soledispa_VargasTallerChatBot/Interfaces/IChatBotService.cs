d35e984b6a846de120a5450ca61e4f731127586b
eduardo <eduardo.andrade.cedeno@udla.edu.ec>
2025-05-14 23:08:00 -0500

    Interfaz IChatBotService creada

---
+++ Andrade_Soledispa_VargasTallerChatBot/Interfaces/IChatBotService.cs
@@ -0,0 +1,8 @@
+﻿namespace Andrade_Soledispa_VargasTallerChatBot.Interfaces
+{
+    public interface IChatBotService
+    {
+        Task<string> ObtenerRespuestaDeGemini(string pregunta);
+        Task<string> ObtenerRespuestaDeChatGPT(string pregunta);
+    }
+}
